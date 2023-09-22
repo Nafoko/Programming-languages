5fda371000743593c48ba09d2a2986182c649f0d
dmitriev <nafoko@mail.ru>
2023-09-23 00:18:32 +0300

    Задача 4 выполнена

---
+++ Task04/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4: Напишите программу, 
+// которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Программа для нахождения максимального сисла из трех. Введите первое число");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberc = Convert.ToInt32(Console.ReadLine());
+
+int max = numbera;
+if (numberb > numbera) max = numberb;
+if (numberc > numberb) max = numberc;
+Console.WriteLine($"{max} - максимальное число");
